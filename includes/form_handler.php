7bc71c90909fabad30ae2820fdc689162c1aed61
lawrenceee04 <lawrencelg04@gmail.com>
2024-10-03 15:08:07 +0800

    changed html to php

---
+++ includes/form_handler.php
@@ -0,0 +1,19 @@
+<?php
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    require_once 'db_connect.php';
+
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $body = $_POST['body'];
+
+    $sql = "INSERT INTO phpfeedbackstart.feedback (name, email, body) VALUES ('$name', '$email', '$body')";
+
+    if ($conn->query($sql) === TRUE) {
+        echo 'New record created successfully';
+    } else {
+        echo 'Error: ' . $sql . '<br>' . $conn->error;
+    }
+
+    $conn->close();
+}
